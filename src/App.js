1ca55c951210ae3ae45715b81a64246de3764d4b
kullph <kullaphat01.tr@gmail.com>
2023-03-11 00:42:00 +0700

    infra

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
 import Login from './Login.js';
 import Student from './Student.js' 
-import Student_free from './Student.js' 
 
 function App() {
-  if(true){
-    return(<Login/>);
-  }
-  else{
-    return(<Student_free/>);
-  }
+  
 }
 
 export default App;
