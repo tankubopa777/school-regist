c380da9530adcf469fe55e32a0dcbeec75374982
kullph <kullaphat01.tr@gmail.com>
2023-03-11 00:42:52 +0700

    infra0.0.1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//router
+import { BrowserRouter, Route, Link, Router, Redirect } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
       <App />
