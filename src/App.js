b3e1f0d054fb11e71371adafe84a700905cf4e20
INTHAT SRIPHAO <86417460+Sosnox@users.noreply.github.com>
2023-03-17 03:12:34 +0700

    update.readfile

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import './App.css';
-import Login_page from './page/Login.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home_page from './page/Home.js';
-import Registered_page from './page/Registered.js';
-//import Student from './Student.js' 
+// import './App.css';
+// import Login_page from './page/Login.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Home_page from './page/Home.js';
+// import Registered_page from './page/Registered.js';
+// //import Student from './Student.js' 
 
-function App() {
-    return (
-    <div>
-        <Home_page />
-    </div>
-    );
+import * as XLSX from 'xlsx';
+import xlsx_file from "./clearDATA.xlsx";
+
+function xlsxArrayBufferToBlob(arrayBuffer) {
+    const mimeType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+    const blob = new Blob([arrayBuffer], { type: mimeType });
+    return blob;
+  }
+
+function readXlsxFile(file) {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      const blob = xlsxArrayBufferToBlob(file)
+      fileReader.readAsArrayBuffer(blob);
+  
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+        const wb = XLSX.read(bufferArray, { type: 'buffer' });
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        const data = XLSX.utils.sheet_to_json(ws);
+        resolve(data);
+      };
+  
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  
+    return promise;
+  }
+
+export default function App() {
+    readXlsxFile(xlsx_file).then((data) => {
+      console.log(data);
+    });
 }
-
-export default App;
