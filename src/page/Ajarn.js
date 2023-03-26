89535637f3c2ecf2e35b9eb58e2da89e6b2cb1ec
INTHAT SRIPHAO <86417460+Sosnox@users.noreply.github.com>
2023-03-27 02:22:43 +0700

    Update.Table.NavAJ

---
+++ src/page/Ajarn.js
@@ -0,0 +1,11 @@
+import NavbarAJ from "../components/NavbarAJ";
+import Table from "../components/Table";
+
+function Ajarn_Page() {
+  return (
+    <div>
+      <NavbarAJ />
+    </div>
+  );
+}
+export default Ajarn_Page;
