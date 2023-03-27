eea4d7daa49e0fad782e073af8215c33eb5d3ff0
INTHAT SRIPHAO <86417460+Sosnox@users.noreply.github.com>
2023-03-27 22:07:31 +0700

    Update.PageFinished

--- src/page/Ajarn.js
+++ src/page/Ajarn.js
@@ -3,33 +3,20 @@ import Table from "../components/Table";
 import Register_subject from "../components/RegSubject";
 import Card_Edit from "../components/Card_Edit";
 import TableStd from "../components/TableStd";
+import Grade from "../components/Grade";
+import EditComponents from "../components/EditComponents";
 
 function Ajarn_Page() {
   return (
     <div>
       <NavbarAJ />
 
+      {/* <Table />  */}
+      {/* <EditComponents /> */}
+      {/*   */}
 
-      {/* edit_page */}
-      <div>
-        <div className="grid grid-cols-4">
-          <div className="m-4 col-span-3">
-            <TableStd />
-          </div>
-          <div className="m-4 mt-5 col-span-1">
-            <Card_Edit />
-          </div>
-        </div>
-        <div className="block float-right mx-5">
-          <button className="bg-green-600 rounded-md px-2 py-1 mx-5 my-3 text-slate-50 hover:bg-lime-700">
-            export
-          </button>
-
-          <button className="bg-green-600 rounded-md px-2 py-1 mx-5 my-3 text-slate-50 hover:bg-lime-700 ">
-            Submit
-          </button>
-        </div>  
-      </div>
+      {/* <EditComponents /> */}
+      
 
 
     </div>
