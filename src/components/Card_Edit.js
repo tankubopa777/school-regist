062189d6ae3673b49fa110a4746707bb06b31c6c
INTHAT SRIPHAO <86417460+Sosnox@users.noreply.github.com>
2023-04-06 16:33:58 +0700

    Update Show Status Subject

--- src/components/Card_Edit.js
+++ src/components/Card_Edit.js
@@ -4,10 +4,19 @@ function Card_Edit(props) {
     function handleClick() { 
         props.updateTablePage('Register_subject');
     }
+    const Status = (status) => {
+        if (status === 'true') {
+            return <div className="bg-green-500 px-2 py-1 text-white rounded-lg">Open</div>
+        }
+        else {
+            return <div className="bg-red-500 px-2 py-1 text-white rounded-lg">Close</div>
+        }
+    }
 
     return (
         <div className="inline-block rounded-lg bg-gray-200 m-4 p-4 w-22 h-23">
             <div>
+                <p className="text-start text-xs" >{Status(props.subjectselect.AVAILABILITY)}</p>
                 <p className="text-start text-xs" > <label className="font-bold">ชื่อชุมนุม/เสรี : </label>{props.subjectselect.SUB_NAME}</p>
                 <p className="text-start text-xs" > <label className="font-bold">คุณครู : </label>{props.subjectselect.SUB_PROF}</p>
                 <p className="text-start text-xs" > <label className="font-bold">จำนวนนักเรียนที่ลงทะเบียน :  </label>{selected[7].length} / {props.subjectselect.SUB_CAP}</p>
--- src/components/Table.js
+++ src/components/Table.js
@@ -17,7 +17,7 @@ function Table(props) {
     }
 
     const toAddSubject = (s) => {
-        props.updatesubjectselect(s);
+        props.updatesubjectselect(s); //ไม่เเน่ใจว่าต้องส่ง parameter ไหม
         props.updateTablePage('Register_subject');
     }
 
@@ -29,6 +29,16 @@ function Table(props) {
             return subjectlst.SUB_TYPE === SubType;
         } 
       });
+
+      const Status = (status) => {
+        if (status === 'true') {
+            return <div className="bg-green-500 px-2 py-1 text-white rounded-lg">Open</div>
+        }
+        else {
+            return <div className="bg-red-500 px-2 py-1 text-white rounded-lg">Close</div>
+        }
+    }
+
     
     return (
 
@@ -62,6 +72,11 @@ function Table(props) {
                         <table className="rounded-lg shadow-md">
                             <thead >
                                 <tr className="rounded-lg shadow-md">
+                                    <th 
+                                        className="border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
+                                    >
+                                        Status
+                                    </th>
                                     <th 
                                         className="border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider"
                                     >
@@ -100,6 +115,12 @@ function Table(props) {
                                     <tr key={index} className="w-max ">
                                         <td className=" p-4 w-1/6 border-b border-gray-200 bg-white text-sm">
                                             <p className="text-gray-900 whitespace-no-wrap">
+                                                {Status(subjectsType.AVAILABILITY)}
+                                            </p>
+                                        </td>
+
+                                        <td className=" p-4 w-1/6 border-b border-gray-200 bg-white text-sm">
+                                            <p className="text-gray-900 whitespace-no-wrap">
                                                 {subjectsType.SUB_ID}
                                             </p>
                                         </td>
@@ -128,7 +149,6 @@ function Table(props) {
                                             </p>
                                         </td>
 
-
                                         <td
                                             className=" w-1/6 border-b border-gray-200 bg-white text-sm text-left"
                                         >
@@ -141,7 +161,7 @@ function Table(props) {
                     </div>
                 </div>
             </div>
-            <button className="bg-green-600 text-center text-white" onClick={() => toAddSubject(subjects)}>AddSubject</button>
+            <button className="bg-green-600 text-center text-white" onClick={() => toAddSubject()}>AddSubject</button>
         </div>
     );
 }
