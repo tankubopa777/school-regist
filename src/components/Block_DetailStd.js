54b14398bc47b96a00ad93796c8e44e8fbde7a3d
INTHAT SRIPHAO <86417460+Sosnox@users.noreply.github.com>
2023-05-11 16:28:22 +0700

    A

--- src/components/Block_DetailStd.js
+++ src/components/Block_DetailStd.js
@@ -1,6 +1,6 @@
 function Block_DetailStd(props) {
     return (
-        <div className="">
+        <div>
             <div className="flex flex-wrap flex-col rounded-lg bg-gray-200 m-4 p-4">
                 <h1 className="font-bold flex flex-row justify-center" > ข้อมูลนักเรียน  </h1>
                 <div className="" >เลขประจำตัว : {props.user.ID}</div>
