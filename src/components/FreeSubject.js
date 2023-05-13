def6cb83503147efb3a0a5f8d0a4f8a13f0ae593
INTHAT SRIPHAO <86417460+Sosnox@users.noreply.github.com>
2023-05-13 23:19:13 +0700

    1.0.1

--- src/components/FreeSubject.js
+++ src/components/FreeSubject.js
@@ -7,12 +7,17 @@ export default function FreeSubject(props) {
     let freeSubject = [];
 
     for (let i = 0; i < subjects.length; i++) {
+
         if (subjects[i].SUB_TYPE === "FREE" && subjects[i].AVAILABILITY) {
-          subjects[i]["CELLIDX"] = subjects.indexOf(subjects[i])
-          
-          
+            subjects[i]["CELLIDX"] = subjects.indexOf(subjects[i])
+            for(let j = 0; j < subjects[i].SUB_PERM.length; j++){
+                if(subjects[i].SUB_PERM[j] == props.user.STD_CLASS){
+                  freeSubject.push(subjects[i]);
+                }
+            }
         }
     }
+    console.log(subjects)
 
   return (
     <div>
